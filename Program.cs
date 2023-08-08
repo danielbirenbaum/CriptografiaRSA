f8404fafc419371f1504f14be400681fc682aa71
Daniel <dani.b.g.castro@gmail.com>
2023-08-07 23:35:36 -0300

    FirstCommit

--- Program.cs
+++ Program.cs
@@ -1,25 +1,28 @@
-﻿namespace CriptografiaRSA
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CriptografiaRSA
 {
-    static class EncryptoService
-    {
-
-    }
-
-    static class Encryptograph
-    {
-
-    }
-
-    static class Decrypt
-    {
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+
+            Console.WriteLine("Bem-vindo ao Efac - Mensagens, uma empresa criada por Daniel e Bernardo!");
+            Console.WriteLine();
+            Console.Write("Você deseja criptografar(1) ou descriptografar(2) uma mensagem?: ");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    Console.WriteLine("Digite a mensagem que iremos criptografar: ");
+                    string message = Console.ReadLine();
+
+                    break;
+                case 2:
+                    break;
+            }  
         }
     }
 }
